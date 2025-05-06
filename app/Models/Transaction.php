33ddb6a19ec700b63fd688120f1d9e055b010e50
Luis Pedroza-Soto <lps.bizdev@gmail.com>
2025-05-06 15:44:54 -0700

    Configuring relationship between users and transactions models

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
@@ -30,5 +31,10 @@ class Transaction extends Model
             'type' => TransactionType::class,
             'updated_at' => 'immutable_datetime',
         ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
