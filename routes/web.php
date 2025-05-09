b3b1ce5b65cc5e549fa7f6a9f3fb52bdf362ec79
Luis Pedroza-Soto <lps.bizdev@gmail.com>
2025-05-09 00:25:58 -0700

    Changing the default home route to either `login` or `dashboard`

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+    return to_route('login');
+})->middleware('jwt-guest')->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
