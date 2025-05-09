7894598b5123c3b04ab1a2fd05477aa1217f38f4
Luis Pedroza-Soto <lps.bizdev@gmail.com>
2025-05-08 22:25:59 -0700

    Adding default value of `0` to user's wallet balance column

--- database/migrations/2025_05_05_063639_add_wallet_balance_column_to_users_table.php
+++ database/migrations/2025_05_05_063639_add_wallet_balance_column_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->double('wallet_balance')->after('password');
+            $table->double('wallet_balance')->default(0)->after('password');
         });
     }
 
